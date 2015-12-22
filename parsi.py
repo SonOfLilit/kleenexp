fce783e700b0261531289e876067a688fbeea688
Aur Saraf <aur@loris.co.il>
2015-12-22 05:37:32 +0200

    parse ['a']

--- parsi.py
+++ parsi.py
@@ -2,11 +2,14 @@ from parsimonious.grammar import Grammar
 from parsimonious.nodes import NodeVisitor
 from collections import namedtuple
 
-grammar = Grammar('''
-regex           = ( outer )*
+grammar = Grammar(r'''
+regex           = outer*
 outer           = outer_literal / braces
-outer_literal   = ~'[^\[\]]+'
-braces          = '[' + ']'
+outer_literal   = ~r'[^\[\]]+'
+braces          = '[' inner ']'
+inner           = inner_literal*
+inner_literal   = '\'' until_quote '\''
+until_quote     = ~r'[^\']*'
 ''')
 
 Concat = namedtuple('Concat', ['items'])
@@ -34,11 +37,23 @@ class Visitor(NodeVisitor):
     def visit_outer_literal(self, literal, _):
         return Literal(literal.text)
 
-    def visit_braces(self, braces, (_l, _r)):
-        return Concat([])
+    def visit_braces(self, braces, (_l, inner, _r)):
+        return Concat(inner)
+
+    def visit_inner(self, inner, children):
+        return children
+
+    def visit_inner_literal(self, _literal, (_1, literal, _2)):
+        return literal
+
+    def visit_until_quote(self, literal, _):
+        return Literal(literal.text)
 
 def test():
     C, E, O, M, L = Concat, Either, Operator, Macro, Literal
     assert Visitor().parse('') == C([])
     assert Visitor().parse('literal') == C([L('literal')])
     assert Visitor().parse('[]') == C([])
+    assert Visitor().parse('a[]b') == C([L('a'), L('b')])
+    assert Visitor().parse("['literal']") == C([L('literal')])
+    assert Visitor().parse("['']") == C([L('')])
