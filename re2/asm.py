fcd50a966ac84a2ef5b6726794fc44b035d1c349
Aur Saraf <aur@loris.co.il>
2015-12-23 18:09:18 +0200

    asm * + ? {a,b}

--- re2/asm.py
+++ re2/asm.py
@@ -11,5 +11,25 @@ class Literal(Asm):
     def to_regex(self):
         return re.escape(self.string)
 
+class Multiple(Asm):
+    def __init__(self, min, max, is_greedy, sub):
+        self.min = min
+        self.max = max
+        self.is_greedy = is_greedy
+        self.sub = sub
+
+    def to_regex(self):
+        if self.min == 0 and self.max is None:
+            op = '*'
+        elif self.min == 1 and self.max is None:
+            op = '+'
+        elif self.min == 0 and self.max == 1:
+            op = '?'
+        elif self.min == self.max:
+            op = '{%d}' % self.min
+        else:
+            op = '{%s,%s}' % (self.min or '', self.max or '')
+        return self.sub.to_regex() + op
+
 def assemble(asm):
     return asm.to_regex()
--- tests/test_asm.py
+++ tests/test_asm.py
@@ -1,6 +1,15 @@
 import pytest
-from re2.asm import assemble, Literal
+from re2.asm import assemble, Literal, Multiple
 
 def test_literal():
     assert assemble(Literal('abc')) == 'abc'
     assert assemble(Literal('^[a](b)$')) == r'\^\[a\]\(b\)\$'
+
+def test_multiple():
+    assert assemble(Multiple(0, 1, False, Literal('a'))) == 'a?'
+    assert assemble(Multiple(0, None, False, Literal('a'))) == 'a*'
+    assert assemble(Multiple(1, None, False, Literal('a'))) == 'a+'
+    assert assemble(Multiple(2, 2, False, Literal('a'))) == 'a{2}'
+    assert assemble(Multiple(None, 2, False, Literal('a'))) == 'a{,2}'
+    assert assemble(Multiple(2, None, False, Literal('a'))) == 'a{2,}'
+    assert assemble(Multiple(2, 5, False, Literal('a'))) == 'a{2,5}'
