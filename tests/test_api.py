3eafdc7230dbfec9869c8a9031d34bad7cf1d431
Aur Saraf <sonoflilit@gmail.com>
2021-11-27 23:08:54 +0200

    fix a test that fails in pypy, disable python 3.10 in travis

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,6 @@
 import pytest
 from ke import re, compile
-from re import error
+from re import error, escape
 
 def test_re():
     assert re('["a"]') == '(?ms)a'
@@ -141,9 +141,16 @@ def test_escapes():
     assert compile('[#dq #q #t #lb #rb #vertical_tab #formfeed #bell #backspace #el]').match('''"'\t[]\v\f\a\b''')
 
 def test_define_macros():
+    expected = ('(?ms)Yo dawg, I heard you like Yo dawg, I heard you like this, so I put some of this in your regex so you can recurse while you recurse, so I put some Yo dawg, I heard you like this, so I put some of this in your regex so you can recurse while you recurse in your Yo dawg, I heard you like this, so I put some of this in your regex so you can recurse while you recurse so you can recurse while you recurse'
+        .replace(' ', '\\ ')
+        .replace(',', escape(',')) # `,` in CPython, `\,` in PyPy
+    )
     assert re('''[#recursive_dawg][
-    #yo=["Yo dawg, I heard you like "] #so_i_put=[", so I put some "] #in_your=[" in your "] #so_you_can=[" so you can "] #while_you=[" while you "]
-    #dawg=[#yo "this" #so_i_put "of this" #in_your "regex" #so_you_can "recurse" #while_you "recurse"]
-    #recursive_dawg=[#yo #dawg #so_i_put #dawg #in_your #dawg #so_you_can "recurse" #while_you "recurse"] ]'''
-    ) == ('(?ms)Yo dawg, I heard you like Yo dawg, I heard you like this, so I put some of this in your regex so you can recurse while you recurse, so I put some Yo dawg, I heard you like this, so I put some of this in your regex so you can recurse while you recurse in your Yo dawg, I heard you like this, so I put some of this in your regex so you can recurse while you recurse so you can recurse while you recurse'
-        .replace(' ', '\\ '))
+        #yo=["Yo dawg, I heard you like "]
+        #so_i_put=[", so I put some "]
+        #in_your=[" in your "]
+        #so_you_can=[" so you can "]
+        #while_you=[" while you "]
+        #dawg=[#yo "this" #so_i_put "of this" #in_your "regex" #so_you_can "recurse" #while_you "recurse"]
+        #recursive_dawg=[#yo #dawg #so_i_put #dawg #in_your #dawg #so_you_can "recurse" #while_you "recurse"]
+    ]''') == expected
