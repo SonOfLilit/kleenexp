71f7508824ae78810c19f7f72112e14644a76e5f
Aur Saraf <sonoflilit@gmail.com>
2022-05-18 17:36:36 +0300

    access log example

--- demo/client/editor.ts
+++ demo/client/editor.ts
@@ -2,13 +2,14 @@ import { EditorState } from "@codemirror/state";
 import { EditorView } from "@codemirror/view";
 import { search, openSearchPanel } from "@codemirror/search";
 import alice from "./alice.txt";
+import accesslog from "./accesslog.txt";
 
 let path = window.location.pathname
 let regexp = path.endsWith('/regex/')
 let kleenexp = path.endsWith('/kleenexp/')
 let lock = regexp || kleenexp
 
-let startState = EditorState.create({
+let aliceStartState = EditorState.create({
   doc: alice,
   extensions: [
     search({ top: true, caseSensitive: true, regexp, kleenexp: kleenexp || !lock, lock }),
@@ -17,9 +18,26 @@ let startState = EditorState.create({
   ],
 });
 
-let view = new EditorView({
-  state: startState,
-  parent: document.body,
+let aliceView = new EditorView({
+  state: aliceStartState,
+  parent: document.getElementById('alice'),
 });
 
-openSearchPanel(view);
+openSearchPanel(aliceView);
+
+
+let accesslogStartState = EditorState.create({
+  doc: accesslog,
+  extensions: [
+    search({ top: true, caseSensitive: true, regexp, kleenexp: kleenexp || !lock, lock }),
+    EditorState.readOnly.of(lock),
+    EditorView.editable.of(!lock),
+  ],
+});
+
+let accesslogView = new EditorView({
+  state: aliceStartState,
+  parent: document.getElementById('accesslog'),
+});
+
+openSearchPanel(accesslogView);
