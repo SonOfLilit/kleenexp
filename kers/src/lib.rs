9c68235a62c53b0914e4210f59ec380f5ee82d47
Aur Saraf <aur@sarafconsulting.com>
2022-08-05 14:40:57 +0300

    kers: first test

--- kers/src/lib.rs
+++ kers/src/lib.rs
@@ -1,3 +1,13 @@
 pub fn transpile(text: &str) -> String {
     return text.to_string();
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn no_braces() {
+        assert_eq!(transpile("abc def!"), "abc def!");
+    }
+}
