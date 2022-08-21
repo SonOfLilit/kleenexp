937ce4f6d9f587e764f1b8cd1a19b73e08724e63
Aur Saraf <aur@sarafconsulting.com>
2022-08-21 19:14:46 +0300

    finally parsing complex operators

--- rust/src/parse.rs
+++ rust/src/parse.rs
@@ -1,5 +1,6 @@
 use pest::error::Error;
 use pest::iterators::Pair;
+use pest::iterators::Pairs;
 use pest::Parser;
 
 #[derive(Parser)]
@@ -25,52 +26,74 @@ pub enum Ast<'a> {
 
 pub fn parse(pattern: &str) -> Result<Ast, Error<Rule>> {
     let rule = KleenexpParser::parse(Rule::kleenexp, pattern)?.next();
-    match rule {
-        Some(r) => Ok(parse_rule(r)),
-        None => Ok(Ast::Concat(vec![])),
-    }
+    Ok(parse_rule(rule))
 }
 
-fn parse_rule(pair: Pair<Rule>) -> Ast {
-    match pair.as_rule() {
-        Rule::braces => {
-            let mut pair = pair.into_inner();
-            match pair.next() {
-                Some(pair) => parse_rule(pair),
-                None => Ast::Concat(vec![]),
+fn parse_rule(pair: Option<Pair<Rule>>) -> Ast {
+    match pair {
+        None => Ast::Concat(vec![]),
+        Some(pair) => match pair.as_rule() {
+            Rule::braces => {
+                let mut pair = pair.into_inner();
+                parse_rule(pair.next())
             }
-        }
-        Rule::ops_matches => {
-            let mut pair = pair.into_inner();
-            let mut op = pair.next().unwrap().into_inner();
-            let operator = op.next().unwrap().as_str();
-            let name: &str = op.next().map(|p| p.as_str()).unwrap_or("");
-            Ast::Operator {
-                op: operator,
-                name: name,
-                subexpr: Box::new(parse_rule(pair.next().unwrap())),
+            Rule::ops_matches => {
+                let mut pairs = pair.into_inner();
+                let ops: Vec<(&str, &str)> = pairs
+                    .next()
+                    .unwrap()
+                    .into_inner()
+                    .map(|p| {
+                        println!("{}", p.as_str());
+                        let mut op = p.into_inner();
+                        let operator = op.next().unwrap().as_str();
+                        let name = match op.next() {
+                            Some(p) => p.as_str(),
+                            None => "",
+                        };
+                        (operator, name)
+                    })
+                    .collect();
+                println!("{:?}", ops);
+                let matches = concat_if_needed(
+                    pairs
+                        .next()
+                        .unwrap()
+                        .into_inner()
+                        .map(|p| parse_rule(Some(p)))
+                        .collect(),
+                );
+                println!("{:?}", matches);
+                ops.iter().rfold(matches, |ast, (op, name)| Ast::Operator {
+                    op: op,
+                    name: name,
+                    subexpr: Box::new(ast),
+                })
             }
-        }
-        Rule::op => todo!(),
-        Rule::either => todo!(),
-        Rule::matches => concat_if_needed(pair.into_inner().map(parse_rule).collect()),
-        Rule::macro_ => {
-            let mut p = pair.into_inner();
-            let name = p.next().unwrap().as_str();
-            Ast::Macro(name)
-        }
-        Rule::outer_literal => Ast::Literal(pair.as_str()),
-        Rule::inner_literal => todo!(),
-        Rule::until_quote => todo!(),
-        Rule::until_doublequote => todo!(),
-        Rule::token => Ast::Literal(pair.as_str()),
-        Rule::WHITESPACE
-        | Rule::EOI
-        | Rule::whitespace_characters
-        | Rule::kleenexp
-        | Rule::match_ => {
-            unreachable!()
-        }
+            Rule::ops => unreachable!(),
+            Rule::op => unreachable!(),
+            Rule::either => todo!(),
+            Rule::matches => {
+                concat_if_needed(pair.into_inner().map(Some).map(parse_rule).collect())
+            }
+            Rule::macro_ => {
+                let mut p = pair.into_inner();
+                let name = p.next().unwrap().as_str();
+                Ast::Macro(name)
+            }
+            Rule::outer_literal => Ast::Literal(pair.as_str()),
+            Rule::inner_literal => todo!(),
+            Rule::until_quote => todo!(),
+            Rule::until_doublequote => todo!(),
+            Rule::token => Ast::Literal(pair.as_str()),
+            Rule::WHITESPACE
+            | Rule::EOI
+            | Rule::whitespace_characters
+            | Rule::kleenexp
+            | Rule::match_ => {
+                unreachable!()
+            }
+        },
     }
 }
 
@@ -113,6 +136,9 @@ mod tests {
             Ok(Ast::Concat(vec![Ast::Macro("hello"), Ast::Macro("hi")])),
             super::parse("[#hello#hi]")
         );
+    }
+    #[test]
+    fn parser_result_operator() {
         assert_eq!(
             Ok(Ast::Operator {
                 op: "2+",
@@ -128,6 +154,49 @@ mod tests {
                 subexpr: Box::new(Ast::Macro("hello"))
             }),
             super::parse("[capture:hi #hello]")
+        );
+    }
+
+    #[test]
+    fn parser_result_operators() {
+        assert_eq!(
+            Ok(Ast::Operator {
+                op: "capture",
+                name: "hi",
+                subexpr: Box::new(Ast::Operator {
+                    op: "2+",
+                    name: "",
+                    subexpr: Box::new(Ast::Macro("hello"))
+                })
+            }),
+            super::parse("[capture:hi 2+ #hello]")
+        );
+        assert_eq!(
+            Ok(Ast::Operator {
+                op: "capture",
+                name: "hi",
+                subexpr: Box::new(Ast::Operator {
+                    op: "2+",
+                    name: "",
+                    subexpr: Box::new(Ast::Operator {
+                        op: "op1",
+                        name: "",
+                        subexpr: Box::new(Ast::Operator {
+                            op: "op2",
+                            name: "",
+                            subexpr: Box::new(Ast::Operator {
+                                op: "op3",
+                                name: "yo",
+                                subexpr: Box::new(Ast::Concat(vec![
+                                    Ast::Macro("hello"),
+                                    Ast::Macro("world")
+                                ]))
+                            })
+                        })
+                    })
+                })
+            }),
+            super::parse("[capture:hi 2+ op1 op2 op3:yo #hello #world]")
         );
     }
 }
