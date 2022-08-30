13344329d71b738d8bedbc17fe16da071df110c3
Aur Saraf <aur@sarafconsulting.com>
2022-08-30 03:15:08 +0300

    enable wee_alloc, console_panic_hook()

--- kleenexp-wasm/src/lib.rs
+++ kleenexp-wasm/src/lib.rs
@@ -1,5 +1,6 @@
 mod utils;
 
+use utils::set_panic_hook;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -10,6 +11,7 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 pub fn transpile(pattern: &str) -> Result<String, JsValue> {
+    set_panic_hook();
     kleenexp::transpile(pattern, kleenexp::RegexFlavor::Javascript)
         .map_err(|e| JsValue::from(format!("{:?}", e)))
 }
