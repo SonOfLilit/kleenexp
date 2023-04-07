93ad5ba67f07e6936a9fbdafcfdd5a0636822a57
Aur Saraf <aur@sarafconsulting.com>
2023-04-07 13:36:04 +0300

    ke.error should be re.error, not a subclass of it

--- ke/_errors.py
+++ ke/_errors.py
@@ -1,12 +1,11 @@
-import re as _original_re
+from re import error
 import parsimonious
 
 
-class KleenexpError(_original_re.error):
+class KleenexpError(error):
     pass
 
 
-error = KleenexpError
 # we want to raise the nice parsimonious ParseError with all the explanation,
 # but we also want to raise something that isinstance(x, re.error)...
 class ParseError(parsimonious.ParseError, KleenexpError):
